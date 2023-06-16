c6e32cd7c4bb4020a8c28017afd96f577629a471
daniel <danielrubensdaniel@gmail.com>
2023-06-16 00:53:05 -0300

    controller: blob storage

--- server/controller/index.js
+++ server/controller/index.js
@@ -1,5 +1,22 @@
+import dotenv from 'dotenv'
+import { BlobServiceClient } from '@azure/storage-blob'
+
 const upload = async(req, res) => {
     const image = req.body
+    const blogSas = process.env.BLOBSAS
+    const containerName = "demo";
+    const containerClient = BlobServiceClient.getContainerClient(containerName);
+    try{
+        const promises = [];
+        for(let i of req.body){
+            const blockBlobClient = containerClient.getBlockBlobClient(i.name)
+            promises.push(blockBlobClient)
+        }
+        await Promise.all(promises)
+    }
+    catch(error){
+        console.log({error})
+    }
     return res.status(200).json({message: 'Test'})
 }
 
